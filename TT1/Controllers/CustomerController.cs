567bd76d93e4aea8ff0333c002db38bec968a60d
liceist2008@mail.ru <liceist2008@mail.ru>
2019-07-21 12:43:02 +0600

    restructuring v4

--- TT1/Controllers/CustomerController.cs
+++ TT1/Controllers/CustomerController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,6 +26,35 @@ namespace TT1.Controllers
 			db.Customers.Add(customer);
 			await db.SaveChangesAsync();
 			return RedirectToAction("Index", "Home");
+		}
+
+		[HttpGet]
+		[ActionName("Delete")]
+		public async Task<IActionResult> ConfirmDelete(int? id)
+		{
+			if (id != null)
+			{
+				Customer customer = await db.Customers.FirstOrDefaultAsync(p => p.Id == id);
+				if (customer != null)
+					return View(customer);
+			}
+			return NotFound();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Delete(int? id)
+		{
+			if (id != null)
+			{
+				Customer customer = await db.Customers.FirstOrDefaultAsync(p => p.Id == id);
+				if (customer != null)
+				{
+					db.Customers.Remove(customer);
+					await db.SaveChangesAsync();
+					return RedirectToAction("Index", "Home");
+				}
+			}
+			return NotFound();
 		}
 	}
 }
--- TT1/Controllers/ProductControllercs.cs
+++ TT1/Controllers/ProductControllercs.cs
@@ -33,5 +33,54 @@ namespace TT1.Controllers
 			await db.SaveChangesAsync();
 			return RedirectToAction("Product");
 		}
+
+		//Редактирование продуктов
+		public async Task<IActionResult> EditProduct(int? id)
+		{
+			if (id != null)
+			{
+				Product product = await db.Products.FirstOrDefaultAsync(p => p.Id == id);
+				if (product != null)
+					return View(product);
+			}
+			return NotFound();
+		}
+		[HttpPost]
+		public async Task<IActionResult> EditProduct(Product product)
+		{
+			db.Products.Update(product);
+			await db.SaveChangesAsync();
+			return RedirectToAction("Product");
+		}
+
+		//Удаление продуктов из БД
+		[HttpGet]
+		[ActionName("DeleteProduct")]
+		public async Task<IActionResult> ConfirmDeleteProduct(int? id)
+		{
+			if (id != null)
+			{
+				Product product = await db.Products.FirstOrDefaultAsync(p => p.Id == id);
+				if (product != null)
+					return View(product);
+			}
+			return NotFound();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> DeleteProduct(int? id)
+		{
+			if (id != null)
+			{
+				Product product = await db.Products.FirstOrDefaultAsync(p => p.Id == id);
+				if (product != null)
+				{
+					db.Products.Remove(product);
+					await db.SaveChangesAsync();
+					return RedirectToAction("Product");
+				}
+			}
+			return NotFound();
+		}
 	}
 }
